22495394c03e6b02972b47332a1a3a1752be96c0
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2026-04-13 23:20:40 +0700

    Application Load Balancer

---
+++ terraform/module/alb/outputs.tf
@@ -0,0 +1,15 @@
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.main.arn
+}
+
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "ec2_sg_id" {
+  value = aws_security_group.ec2_sg.id
+}
